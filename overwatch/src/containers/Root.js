d62a55a94a7bc1a14d02e02fbf17b24785d8de07
grobolom <vasja.volin@gmail.com>
2017-06-18 13:59:19 -0400

    Temporarily protect the teams page with authentication

--- overwatch/src/containers/Root.js
+++ overwatch/src/containers/Root.js
@@ -5,13 +5,14 @@ import { Router, Route, Redirect, browserHistory } from 'react-router'
 
 import Monitor from './Monitor'
 import TeamBuilder from './TeamBuilder'
+import Authenticate from './Authenticate'
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Redirect from="/" to="/monitor" />
       <Route path="/monitor" component={Monitor} />
-      <Route path="/teams" component={TeamBuilder} />
+      <Route path="/teams" component={Authenticate(TeamBuilder)} />
     </Router>
   </Provider>
 )
